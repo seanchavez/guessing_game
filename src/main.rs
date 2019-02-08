2be7e3eb08a8e5275d285f0fb3a5c82390936bfb
Sean Chavez <realseanchavez@gmail.com>
2019-02-08 14:02:43 -0800

    allow multiple guesses and quit after correct

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,28 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please Input your guess.");
+    loop {
+        println!("Please Input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");    
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!()
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");    
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
     
 }
